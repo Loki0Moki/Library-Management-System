8e4a7145971ea7bd0ebf63b9c5e6063d66507fd8
Loki0Moki <shuvo09sa@gmail.com>
2025-05-31 13:04:48 +0600

    new

--- Library management system.my work/Model/user_schema.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE users
-ADD id_proof VARCHAR(255) AFTER email,
-ADD library_card_number VARCHAR(20) DEFAULT NULL AFTER password,
-ADD status ENUM('pending', 'approved') DEFAULT 'pending' AFTER role;
